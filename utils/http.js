389e67bcc5af243fd1330bd355ba3b7e25c07759
YangHanBing <2180244540@qq.com>
2022-08-05 20:08:16 +0800

    meta(新功能)baseUrl封装+loading加载

--- utils/http.js
+++ utils/http.js
@@ -8,24 +8,32 @@ class Http {
   static async request({
     url,
     method = 'GET',
-    data = {}
+    data = {},
+    name = 'api1'
   }, options) {
-    const response = await wxToPromise('request', {
-      url: APIConfig.baseURL + url,
-      data,
-      method,
-      ...options
-    })
-    // 请求成功
-    if (response.statusCode < 400) {
-      return response.data
+    wx.showLoading()
+    try {
+      const response = await wxToPromise('request', {
+        url: APIConfig[name].baseURL + url,
+        data,
+        method,
+        ...options
+      })
+      // 请求成功
+      if (response.statusCode < 400) {
+        return response.data
+      }
+      // token过期处理
+      if (response.statusCode === 401) {
+        return
+      }
+      Http._showError(response.data.code, response.data.msg)
+      return response
+    }catch(error){
+      wx.hideLoading()
+      _showError(-1)
+      console.log(error);
     }
-    // token过期处理
-    if (response.statusCode === 401) {
-      return
-    }
-    Http._showError(response.data.code, response.data.msg)
-    return response
   }
 
   static _showError(code, msg) {
